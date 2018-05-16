a5546a05a1a3ec3afe61682d74b52f6e31bf11f9
BechenLeon3 <leon2.1@gmx.de>
2018-05-16 18:51:04 +0200

    Movement und grundlegende Methoden fürs malen
    Methode für modularverwendbaren 1x 8Bit.
    Methode für malaria (nicht fertig)
    erste Lauf-Methode

--- Basics/Basics/MainForm.Designer.cs
+++ Basics/Basics/MainForm.Designer.cs
@@ -34,10 +34,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(464, 441);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
+            this.ShowIcon = false;
             this.Text = "GDI+ Sample";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.ResumeLayout(false);
 
         }
--- Basics/Basics/MainForm.cs
+++ Basics/Basics/MainForm.cs
@@ -12,6 +12,7 @@ namespace Basics
 {
     public partial class MainForm : Form
     {
+        int ix0 = 0, iy0 = 400, ix1, iy1;
         public MainForm()
         {
             InitializeComponent();
@@ -22,16 +23,70 @@ namespace Basics
         /// </summary>
         /// <param name="e">Contains the graphics object needed to paint.</param>
         protected override void OnPaint(PaintEventArgs e)
-        {
+        { 
             // Has always to be the first line of the overriden OnPaint-Method.
-            base.OnPaint(e);
-            // Get the graphics object.
             Graphics graphics = e.Graphics;
-            // Prepare a Pen and a Rectagle to paint later on.
-            Pen rectaglePen = Pens.Blue;
-            Rectangle rectangle = new Rectangle(10, 20, 100, 50);
-            // Actually draw the Rectagle using the selected Pen.
-            graphics.DrawRectangle(rectaglePen, rectangle);
+            base.OnPaint(e);
+            Malario_Child_Standing(ref graphics, ref ix0, ref iy0);
+
+
+
+
+        }
+
+    
+
+        //Ein Bit : Braucht Füllfarbe, X-/Y-Coordinaten, Randfarbe
+        public void Bit(ref Graphics graphics, ref int ix1, ref int iy1, ref Color GraficColor, ref Color PenColor)
+        {
+
+            SolidBrush Brush = new SolidBrush(GraficColor);
+            Pen pen = new Pen(PenColor);
+            Rectangle Bit = new Rectangle(ix1, iy1, 4, 4);
+            Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+
+            graphics.DrawRectangle(pen, Bit);
+            formGraphics.FillRectangle(Brush, Bit);
+        }
+
+
+
+        protected void Malario_Child_Standing(ref Graphics graphics,ref int ix0, ref int iy0)
+        {
+            ix1 = ix0;
+            iy1 = iy0;
+            Color GraficColor;
+            Color PenColor;
+            GraficColor = Color.Brown;
+            PenColor = Color.Brown;
+
+            Bit(ref graphics, ref ix1, ref iy1, ref GraficColor, ref PenColor);
+        }
+
+
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.D:
+                            ix0++;
+                            break;
+                        case Keys.A:
+                            if (ix0 > 0){ix0--;}
+                            break;
+                        default:
+                        case Keys.Z:
+                            break;
+                    }
+            this.Invalidate();
         }
     }
 }
+
