58cc581ea0049de6095cfbeabbb0f4a7ace7b704
BechenLeon3 <leon2.1@gmx.de>
2018-05-23 11:20:33 +0200

    .
    ,

--- Malario/GDI Malario/GDI Malario/Level_Blöcke.cs
+++ Malario/GDI Malario/GDI Malario/Level_Blöcke.cs
@@ -14,16 +14,16 @@ namespace GDI_Malario
     {
         public static void setzen_Boden(ref int x_Coordinate_PanelG)
         {
-            int y_Coordinate_PanelG = 537 - (24 * 2), x_Panel_Size = 0, y_Panel_Size = 0;
+            int y_Coordinate_PanelG = 489, x_Panel_Size = 24, y_Panel_Size = 24 * 2;
             string Panel_Name = "Panel_Boden_";
             Color Hintergrundfarbe = Color.Gray;
             Grundmethoden grundmethoden = new Grundmethoden();
             grundmethoden.generiert_Panel(ref x_Coordinate_PanelG, ref y_Coordinate_PanelG, ref x_Panel_Size, ref y_Panel_Size, ref Hintergrundfarbe, ref Panel_Name);
+            
         }
-        public static void malen_MauerBlock(ref Graphics graphics, ref int zähler_Blöcke, ref int zähler_Block)
+        public static void malen_MauerBlock(ref Graphics graphics, ref int zähler_Blöcke, ref int zähler_Block, ref int x_Coordinate1, ref int y_Coordinate1)
         {
             int y_Bit_Multiplier_GanzerBlock = 24,
-                x_Coordinate1 = 0, y_Coordinate1 = 0,
                 x_Bit_Multiplier0 = (y_Bit_Multiplier_GanzerBlock - 8) / 2,
                 y_Bit_Multiplier0 = (y_Bit_Multiplier_GanzerBlock - 16) / 4,
                 Block_Spalenbreite = y_Bit_Multiplier_GanzerBlock / 12;
