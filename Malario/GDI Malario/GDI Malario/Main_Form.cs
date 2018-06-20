d85a7830ab98ba1185aea2e209ba5030c114ed4a
BechenLeon3 <38724135+BechenLeon3@users.noreply.github.com>
2018-06-20 22:44:42 +0200

    Hauptmenü ansatz
    OnPaint oder Timer Lag entstanden.!

--- Malario/GDI Malario/GDI Malario/Main_Form.cs
+++ Malario/GDI Malario/GDI Malario/Main_Form.cs
@@ -12,28 +12,53 @@ namespace GDI_Malario
 {
     public partial class Main_Form : Form
     {
-        List<Panel> gemalteslist = new List<Panel>();
-        int Panelanzahl_Zähler;
-        bool M_Right = false, M_Left = false, M_Crouch = false, M_Jump = false;
-        int anziehungskraft, anziehungskaft_Wert = 15, x_Pos_Malario = 0, y_Pos_Malario = 0;
+        List<int> gemalteslist_x_Pos = new List<int>();
+        List<int> gemalteslist_y_Pos = new List<int>();
+        List<int> gemalteslist_Blockart = new List<int>();
+        bool M_Right = false, M_Left = false, M_Crouch = false, M_Jump = false, Startbildschirm = true;
+        int anziehungskraft, anziehungskaft_Wert = 15, x_Pos_Malario = 0, y_Pos_Malario = 0, x_Pos_Block = 0, y_Pos_Block = 0;
 
-        private void Main_Form_Paint_1(object sender, PaintEventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
         {
             Graphics graphics = e.Graphics;
+            base.OnPaint(e);
 
+
+
+            label1counter.Text = Convert.ToString(gemalteslist_x_Pos.Count);
             Malario.malen_Malario(graphics, x_Pos_Malario, y_Pos_Malario);
+            int Blocklist_Zähler = 0;
+
+            if (Startbildschirm == true)
+            {
+                malen_Startmenü(graphics);
+                Startbildschirm = false;
+            }
+
+            if (gemalteslist_x_Pos.Count > 0)
+            {
+                do
+                {
+                    if (gemalteslist_Blockart[Blocklist_Zähler] == 0)
+                    {
+                        Level_Blöcke.malen_BodenBlock(graphics, gemalteslist_x_Pos[Blocklist_Zähler], gemalteslist_y_Pos[Blocklist_Zähler]);
+                        Level_Blöcke.malen_BodenBlock(graphics, gemalteslist_x_Pos[Blocklist_Zähler], gemalteslist_y_Pos[Blocklist_Zähler] + 24);
+                        Blocklist_Zähler += 1;
+                    }
+                } while (Blocklist_Zähler < gemalteslist_Blockart.Count);
+            }
+        }
+        private void Main_Form_Paint_1(object sender, PaintEventArgs e)
+        {
         }
 
         public Main_Form()
         {
-            this.DoubleBuffered = true;
             InitializeComponent();
         }
         private void Main_Form_Load(object sender, EventArgs e)
         {
-            this.DoubleBuffered = true;
         }
-
         private void Main_Form_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
@@ -55,38 +80,42 @@ namespace GDI_Malario
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Invalidate(); 
+             
             if (M_Right == true)
             {
-                
-                if(x_Pos_Malario < (this.Width / 2)){ x_Pos_Malario += 4; }
- /*               else {
-                    int Bewegungs_Panel_Zähler = panellist.Count-1;
+                Invalidate();
+
+                if (x_Pos_Malario < (this.Width / 2)){ x_Pos_Malario += 4; }
+               else {
+                    
+                    int Bewegungs_Panel_Zähler = gemalteslist_x_Pos.Count-1;
                     do
                     {
-                       panellist[Bewegungs_Panel_Zähler].Left -= 4;
-                        panellist[Bewegungs_Panel_Zähler].Invalidate();
-                       Bewegungs_Panel_Zähler--;
-                    } while(Bewegungs_Panel_Zähler>=0);
-               } */
+                        gemalteslist_x_Pos[Bewegungs_Panel_Zähler]-= 4;
+                        Bewegungs_Panel_Zähler--;
+                    } while(Bewegungs_Panel_Zähler>0);
+               }
             }
             if (M_Left == true && x_Pos_Malario > 0)
             {
+                Invalidate();
                 x_Pos_Malario -= 4;
             }
             if (M_Jump == true)
             {
+                Invalidate();
                 y_Pos_Malario -= anziehungskraft;
                 anziehungskraft -= 1;
             }
-            if (32 + y_Pos_Malario >= this.Height - 50)
+            if (y_Pos_Malario >= this.Height - 32 - 39 - 50)
             {
-                y_Pos_Malario = this.Height - 32 - 50;
+                y_Pos_Malario = this.Height - 32 - 39 - 50;
 
                 // 32 = Malariogröße
                 // 50 = der Boden unter Malario (Hardcode im Collisiongrit ändern!)
                 M_Jump = false;
                 anziehungskraft = anziehungskaft_Wert;
+                Invalidate();
             }
         }
 
@@ -101,6 +130,24 @@ namespace GDI_Malario
                     M_Left = false;
                     break;
             }
+        }
+        private void malen_Startmenü(Graphics graphics)
+        {
+            x_Pos_Block = 0;
+            y_Pos_Block = (this.Height - 39 - 48);
+            do
+            {
+                Level_Blöcke.malen_BodenBlock(graphics, x_Pos_Block, y_Pos_Block);
+                gemalteslist_Blockart.Add(0);
+                gemalteslist_x_Pos.Add(x_Pos_Block);
+                gemalteslist_y_Pos.Add(y_Pos_Block);
+                y_Pos_Block += 24;
+
+                Level_Blöcke.malen_BodenBlock(graphics, x_Pos_Block, y_Pos_Block);
+                y_Pos_Block -= 24;
+                x_Pos_Block += 24;
+            } while (x_Pos_Block < 480);
+            Invalidate();
         }
     }
 }
